e20174d97a6b197155a2fa946876bbf415752939
Gabe <fullstackgabe@gmail.com>
2026-09-19 18:03:07 -0300

    Botão do paywall leva pro WhatsApp

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ export const isDemo = !import.meta.env.VITE_SUPABASE_PROJECT_URL
 
 export const DEMO_UID = 'demo-user-local'
 
-export const SUBSCRIBE_URL = 'https://www.instagram.com/bielcwb/'
+export const SUBSCRIBE_URL = 'https://wa.me/554187998159?text=Oi%21%20Quero%20assinar%20o%20Ned%20%F0%9F%A4%96'
